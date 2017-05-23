c9cb821aee5c139c509a095853832c7cdd2ddd87
DaanJG <dj.gilissen@student.han.nl>
2017-05-23 22:43:28 +0200

    test gemaakt

--- WebApp.py
+++ WebApp.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('homepage')
+    return render_template('homepage.html')
 
-@app.route('/dbsearch')
+
+@app.route('/dbsearch', methods=['POST', 'GET'])
 def search():
-    resultslist = list()
-    return resultslist
+    # resultslist = list()
+    text = request.form["searchword"]
+    return str(results(text))
 
 
-@app.route('/result')
-def results():
-    return render_template('resultpage')
+def results(text):
+    text = text + "abc"
+    return text
+    # return render_template('resultspage.html')
+
+
+if __name__ == '__main__':
+    app.run()
